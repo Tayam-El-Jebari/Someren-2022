d68e27ae8413e92d190692b747978591ad9cfcc2
LarsHartendorp <690720@student.inholland.nl>
2022-03-24 18:30:00 +0100

    RegisterLogic Laag

---
+++ Someren-master/SomerenLogic/RegisterLogic.cs
@@ -0,0 +1,26 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenLogic
+{
+    public class RegisterLogic
+    {
+        private RegisterDAO registerdb;
+
+        public RegisterLogic()
+        {
+            registerdb = new RegisterDAO();
+        }
+
+        public List<Register> RegisterList() 
+        {
+            List<Register> registers = registerdb.GetAllRegisters();
+            return registers;
+        }
+    }
+}
